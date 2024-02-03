61cf3656bd3d8f7cb7cbbd4a67919fc00a133e7b
MD SAMIM HOSSAIN SUJON <98223435+samimhossainsujon@users.noreply.github.com>
2024-02-03 21:25:23 +0600

    code add

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,23 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => {
+        console.log(data);
+    }
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label htmlFor="id">ID:</label>
+                <input type="text" id="id" {...register('id')} />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input type="text" id="password" {...register('password')} />
+            </div>
+            <Button htmlType="submit">Login</Button>
+        </form>
     );
 };
 
